3166e08ece844e2fee6c8228304848eb07367069
Daniel Mora <daniel.mora@algotive.ai>
2023-08-16 15:37:39 -0600

    chore: remove .env and .lock.json to the project

--- controllers/demo-bot.js
+++ controllers/demo-bot.js
@@ -1,6 +1,7 @@
-const { TelegramBot } = require('telegramsjs')
+const { TelegramBot, Markup } = require('telegramsjs')
 
 const bot = new TelegramBot(process.env.TG_TOKEN)
+bot.use({})
 
 bot.on('ready', async (client) => {
   bot.setMyCommands({
@@ -10,13 +11,10 @@ bot.on('ready', async (client) => {
         description: 'starting command'
       },
       {
-        command: '/remove',
-        description: 'remove session'
+        command: '/direcciones',
+        description: 'Directorio de direcciones de la Fiscalía del estado'
       },
-      {
-        command: '/stats',
-        description: 'statistics session'
-      },
+
       { command: '/horarios', description: 'Horario de las dependencias' }
     ]
   })
@@ -24,24 +22,64 @@ bot.on('ready', async (client) => {
   console.log(`Starting ${client.username}`)
 })
 
-bot.use({})
-
-bot.on('message', (ctx) => {
-  bot.session.counter = bot.session.counter || 0
-  bot.session.counter++
-  ctx.replyWithMarkdownV2(
-    `Counter updated, new value: \`${bot.session.counter}\``
-  )
-})
+const markup = {
+  inline_keyboard: [[Markup.callback('Fiscalía Estatal', 'core'), Markup.callback('Personas Desaparecidas', 'fepd'), Markup.callback('Delitos contra las Mujeres', 'fedm')]]
+}
 
 bot.command('start', (ctx) => {
-  ctx.replyWithMarkdown(' *thanks for using telegramsjs ❤️*')
-  console.log(ctx.util.checkLocation())
+  ctx.reply('Bienvenido al Asistente Digital de la Fiscalía del Estado de Jalisco')
+}, 'private')
+
+bot.command('direcciones', (ctx) => {
+  ctx.reply('Por Favor selecciona la dependencia de tu interés', {
+    reply_markup: markup
+  })
+}, 'private')
+
+bot.action('core', (ctx) => {
+  const username = ctx.from.first_name
+  ctx.reply(`${username}, La Fiscalía Estatal esta ubicada en la calle 14, colonia industrial, aquí tienes las indicaciones`)
+  bot.sendLocation({
+    chat_id: ctx.from.id,
+    latitude: 20.6390935,
+    longitude: -103.3645254
+  })
+}, true)
+
+/*
+// Sending a document file using an HTTP URL
+bot.sendDocument({
+  chat_id: 913700,
+  document: 'https://www.redalyc.org/pdf/439/43970102.pdf'
+}).then((data) => {
+  console.log(data)
 })
 
-bot.command('horarios', (ctx) => {
-  ctx.replyWithMarkdown('horarols dasdsa')
-  console.log('mandando horarios')
+bot.sendLocation({
+  chat_id: 913700,
+  latitude: 40.7128,
+  longitude: -74.0060
+}).then((data) => {
+  console.log(data)
 })
 
+*/
+
+// Uploading a new document file
+// Please note that this is just an example and the implementation may vary based on your programming environment.
+/*
+const fs = require('fs')
+const documentFilePath = 'file.pdf'
+const documentStream = fs.createReadStream(documentFilePath)
+
+if (documentStream) {
+  console.log('se subió info')
+}
+
+bot.sendDocument({
+  chat_id: 913700,
+  document: documentStream
+}).then((data) => {
+  console.log(data)
+}) */
 bot.login()
